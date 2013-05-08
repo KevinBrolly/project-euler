b92d8c446fc6cf69a1f595abae9448a158fe5ca0
Kevin Brolly <kevin.brolly@gmail.com>
2013-05-08 12:09:41 +0100

    Added some alternate solutions to problem 1

--- problem_1.py
+++ problem_1.py
@@ -5,9 +5,45 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
+import time
+
+#Attempt 1
+
+start_time = time.time()
+
 total = 0
 for i in range(1, 1000):
     if i % 3 == 0 or i % 5 == 0:
         total = total + i
 
 print total
+
+print time.time() - start_time, "seconds\n"
+
+"""
+These 'improved' versions are only really more efficient with numbers
+larger than 1000
+"""
+
+#Improvement 1
+
+start_time = time.time()
+
+total = 0
+for i in xrange(1, 1000):
+    if i % 3 == 0 or i % 5 == 0:
+        total = total + i
+
+print total
+
+print time.time() - start_time, "seconds\n"
+
+#Improvement 2
+
+start_time = time.time()
+
+total = sum([i for i in xrange(1, 1000) if i % 3 == 0 or i % 5 == 0])
+
+print total
+
+print time.time() - start_time, "seconds\n"
